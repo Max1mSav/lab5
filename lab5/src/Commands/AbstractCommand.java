37f4d073bed41059ebb5a92d9f553261cce4016d
Max1mSav <73459495+Max1mSav@users.noreply.github.com>
2021-05-24 00:34:46 +0300

    Add files via upload

---
+++ lab5/src/Commands/AbstractCommand.java
@@ -0,0 +1,20 @@
+/*package Commands;
+
+public abstract class AbstractCommand implements Command {
+    private String name;
+    private String task;
+
+    public AbstractCommand(String name, String destiny) {
+        this.name = name;
+        this.task = task;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDestiny() {
+         return destiny;
+     }
+} */
